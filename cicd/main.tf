7417f2751caf8b43d7bcc1995db79c7197c7b6a4
AvinDevops <avinashbandela73@gmail.com>
2025-09-04 22:18:36 +0530

    Jenkins master, agent and nexus

--- cicd/main.tf
+++ cicd/main.tf
@@ -32,6 +32,35 @@ module "jenkins_agent" {
   }
 }
 
+### creating Nexus Instance ###
+## importing public key ##
+resource "aws_key_pair" "tools" {
+  key_name = "tools"
+  public_key = file("~/.ssh/tools.pub")
+}
+
+module "nexus" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+
+  name = "nexus"
+
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = ["sg-023ff420fac1e729a"]
+  subnet_id = "subnet-0cd5b9a7016bd1324"
+  ami = data.aws_ami.ami_info.id
+  create_security_group = false
+  key_name = aws_key_pair.tools.key_name
+  root_block_device = [
+    {
+      volume_type = "gp3"
+      volume_size = 30
+    }
+  ]
+  tags = {
+    Name = "nexus"
+  }
+}
+
 ### creating r53 records master & agent ###
 module "records" {
   source  = "terraform-aws-modules/route53/aws//modules/records"
@@ -55,6 +84,15 @@ module "records" {
       ttl     = 1
       records = [
         module.jenkins_agent.private_ip
+      ]
+      allow_overwrite = true
+    },
+    {
+      name    = "nexus" 
+      type    = "A"
+      ttl     = 1
+      records = [
+        module.nexus.private_ip
       ]
       allow_overwrite = true
     }
